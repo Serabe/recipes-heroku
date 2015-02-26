5da08e8e20c7dec3d83cea5dd996a0bee8805fde
Sergio Arbeo <serabe@gmail.com>
2015-02-26 01:18:54 +0100

    Add major refactoring to ingredient controller.

--- app/controllers/api/ingredients_controller.rb
+++ app/controllers/api/ingredients_controller.rb
@@ -1,33 +1,49 @@
 module API
   class IngredientsController < ApplicationController
 
-    def index
-      render json: Ingredient.all, each_serializer: ::IndexIngredientSerializer
-    end
+    before_action :load_resource, only: [:create, :update]
 
-    def create
-      ingredient = Ingredient.new create_params
-
-      if ingredient.save
-        render json: ingredient, serializer: ::ShowIngredientSerializer
-      else
-        render json: ingredient, serializer: ::ErrorIngredientSerializer, status: :unprocessable_entity
-      end
-    end
-
-    def update
-      ingredient = Ingredient.find params[:id]
-
-      if ingredient.update update_params
-        render json: ingredient, serializer: ::ShowIngredientSerializer
-      else
-        render json: ingredient, serializer: ::ErrorIngredientSerializer, status: :unprocessable_entity
-      end
-    rescue ActiveRecord::RecordNotFound
+    rescue_from ActiveRecord::RecordNotFound do |exception|
       head :not_found
     end
 
+    def index
+      render json: Ingredient.all,
+             each_serializer: ::IndexIngredientSerializer
+    end
+
+    def create
+      @resource.save
+      render_resource
+    end
+
+    def update
+      @resource.update update_params
+      render_resource
+    end
+
     protected
+
+    def render_resource
+      render json: @resource,
+             status: status_code
+    end
+
+    def load_resource
+      @resource = if action_name == 'create'
+                    Ingredient.new create_params
+                  else
+                    Ingredient.find params[:id]
+                  end
+    end
+
+    def status_code
+      if @resource.valid?
+        action_name == 'create' ? :created : :ok
+      else
+        :unprocessable_entity
+      end
+    end
 
     def create_params
       params.require(:ingredient).permit(:name)
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,11 @@
 class Ingredient < ActiveRecord::Base
   validates :name, presence: true
+
+  def active_model_serializer
+    if self.valid?
+      ::ShowIngredientSerializer
+    else
+      ::ErrorIngredientSerializer
+    end
+  end
 end
