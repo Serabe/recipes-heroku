a388975f0c92b95b41b1e9752799b91ecbdd0649
Sergio Arbeo <serabe@gmail.com>
2015-03-13 09:26:03 +0100

    Revert "Remove devise."
    This reverts commit 92f987847e632601bd7485931d634bbe9969747a.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :steps
     resources :recipes
   end
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
